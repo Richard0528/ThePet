6d52479632aaf787f209b81d8025f9c2a9a5bed2
Zhihao Yang <monester@uw.edu>
2018-08-19 20:25:44 -0700

    login activity added

--- app/src/main/java/www/petapp/com/thepet/HomeFragment.java
+++ app/src/main/java/www/petapp/com/thepet/HomeFragment.java
@@ -4,6 +4,7 @@ package www.petapp.com.thepet;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
+import www.petapp.com.thepet.model.EndlessRecyclerViewScrollListener;
 import www.petapp.com.thepet.model.PetCardItem;
 import www.petapp.com.thepet.model.RecommendedRecycleViewAdapter;
 
@@ -21,14 +23,18 @@ import www.petapp.com.thepet.model.RecommendedRecycleViewAdapter;
  */
 public class HomeFragment extends Fragment {
 
+    // recommended section
     private RecyclerView mRecommendedPetRecycleView;
     private ArrayList<PetCardItem> mRecommendedPetList;
     private RecommendedRecycleViewAdapter homeRecommendedPetAdapter;
 
+    // explore section
+    private EndlessRecyclerViewScrollListener scrollListener;
+    private RecyclerView exploreItemRecycleView;
+
     public HomeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,13 +43,20 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         mRecommendedPetRecycleView = view.findViewById(R.id.home_recommended);
+        exploreItemRecycleView = view.findViewById(R.id.home_explore);
         return view;
     }
 
     public void onStart() {
         super.onStart();
 
-        // pet news
+        setUpRecommended();
+
+        setUpExplore();
+    }
+
+    private void setUpRecommended() {
+        // Recommended section
         mRecommendedPetList = new ArrayList<>();
         mRecommendedPetList.add(new PetCardItem(R.drawable.images_1, "First Pet", "Age, month"));
         mRecommendedPetList.add(new PetCardItem(R.drawable.images_2, "Second Pet", "Age, month"));
@@ -56,7 +69,30 @@ public class HomeFragment extends Fragment {
 
         homeRecommendedPetAdapter = new RecommendedRecycleViewAdapter(mRecommendedPetList);
         mRecommendedPetRecycleView.setAdapter(homeRecommendedPetAdapter);
+    }
 
+    private void setUpExplore() {
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        exploreItemRecycleView.setLayoutManager(gridLayoutManager);
+        scrollListener = new EndlessRecyclerViewScrollListener(gridLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                loadNextDataFromApi(page);
+            }
+        };
+
+        exploreItemRecycleView.addOnScrollListener(scrollListener);
+    }
+
+    public void loadNextDataFromApi(int offset) {
+        // Send an API request to retrieve appropriate paginated data
+        //  --> Send the request including an offset value (i.e `page`) as a query parameter.
+        //  --> Deserialize and construct new model objects from the API response
+        //  --> Append the new data objects to the existing set of items inside the array of items
+        //  --> Notify the adapter of the new items made with `notifyItemRangeInserted()`
     }
 
     public interface OnFragmentInteractionListener {
---
+++ app/src/main/java/www/petapp/com/thepet/login/LoginActivity.java
@@ -0,0 +1,189 @@
+package www.petapp.com.thepet.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import www.petapp.com.thepet.MainActivity;
+import www.petapp.com.thepet.R;
+
+import static android.text.TextUtils.isEmpty;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    // for firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    // set up the layout
+    private ImageView mLogo;
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button mLogin;
+//    private TextView mRegister;
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        mLogo = findViewById(R.id.logo);
+        mEmail = findViewById(R.id.input_email);
+        mPassword = findViewById(R.id.input_password);
+        mLogin = findViewById(R.id.btn_login);
+//        mRegister = findViewById(R.id.link_register);
+
+
+        initProgressBar();
+        setupFirebaseAuth();
+        init();
+    }
+
+    private void init() {
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //check if the fields are filled out
+                if(!isEmpty(mEmail.getText().toString())
+                        && !isEmpty(mPassword.getText().toString())){
+                    Log.d(TAG, "onClick: attempting to authenticate.");
+
+                    showProgressBar();
+
+                    mAuth.signInWithEmailAndPassword(mEmail.getText().toString(),
+                            mPassword.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    hideProgressBar();
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                    hideProgressBar();
+                                }
+                            });
+                }else{
+                    Toast.makeText(LoginActivity.this, "You didn't fill in all the fields.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+//        mRegister.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d(TAG, "onClick: Navigating to Register Screen");
+//
+//                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+//
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+
+    private void showProgressBar(){
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void initProgressBar(){
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
+    /*
+        ----------------------------- Firebase setup ---------------------------------
+     */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: started");
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                    //check if email is verified
+                    if(user.isEmailVerified()){
+                        Log.d(TAG, "onAuthStateChanged: signed_in: " + user.getUid());
+                        Toast.makeText(LoginActivity.this, "Authenticated with: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+
+//                        Intent intent = new Intent(LoginActivity.this, SearchActivity.class);
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                        startActivity(intent);
+//                        finish();
+//                        finish();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.putExtra("toOpen", "ProfileFragment");
+                        startActivity(intent);
+
+//                        ProfileFragment profileFragment = new ProfileFragment();
+//                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction()
+//                                .replace(R.id.login_activity, profileFragment, profileFragment.getTag());
+////                        fragmentTransaction.detach(profileFragment).attach(profileFragment).commit();
+//                        fragmentTransaction.commit();
+
+
+
+
+
+
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Email is not Verified\nCheck your Inbox", Toast.LENGTH_SHORT).show();
+                        FirebaseAuth.getInstance().signOut();
+                    }
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+                // ...
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
